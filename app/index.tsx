51d28c1fb87f0c45f3b35570892542c684f5d852
nbquochuyOmnibox <huy.nguyen@omnibox.com.vn>
2025-01-22 14:48:19 +0700

    feat: Init base project

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center bg-red-50">
+      <Text className="text-3xl font-pblack ">App</Text>
+      <StatusBar style="auto" />
+      <Link href="/+not-found">Not Found</Link>
+    </View>
+  );
+}
