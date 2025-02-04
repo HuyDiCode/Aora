6e54665fe6b644a54a5fbc7548f166729c0f8b9d
nbquochuy <nbquochuy.forwork@gmail.com>
2025-02-04 16:00:44 +0700

    refactor: Update Appwrite integration in lib/appwrite.ts
    This commit updates the Appwrite integration in lib/appwrite.ts. It imports the necessary modules from "react-native-appwrite" and updates the appwriteConfig object with the correct endpoint, project ID, database ID, collection IDs, and storage ID. These changes ensure proper communication with the Appwrite backend and enable features such as user management, sign-in, sign-up, and database operations.

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,14 @@
-export const appwriteConfig = {
+import {
+  Account,
+  Avatars,
+  Client,
+  Databases,
+  ID,
+  Models,
+  Query,
+} from "react-native-appwrite";
+
+export const config = {
   endpoint: "https://cloud.appwrite.io/v1",
   platform: "com.nbquochuy.aora",
   projectId: "67a17a12002fe826ccb0",
@@ -6,4 +16,78 @@ export const appwriteConfig = {
   userCollectionId: "67a17b900005f630d4bb",
   videoCollectionId: "67a17c5400330b90bd09",
   storageId: "67a17db30015bb159891",
+};
+
+const client: Client = new Client()
+  .setEndpoint(config.endpoint)
+  .setProject(config.projectId)
+  .setPlatform(config.platform);
+
+const account: Account = new Account(client);
+const avatars: Avatars = new Avatars(client);
+const databases: Databases = new Databases(client);
+
+export const createUser = async (
+  email: string,
+  password: string,
+  username: string
+) => {
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      username
+    );
+    if (!newAccount) throw Error;
+    const avatarUrl = avatars.getInitials(username);
+
+    await signIn(email, password);
+
+    const newUser = await databases.createDocument(
+      config.databaseId,
+      config.userCollectionId,
+      ID.unique(),
+      {
+        accountId: newAccount.$id,
+        email,
+        username,
+        avatar: avatarUrl,
+      }
+    );
+    return newUser;
+  } catch (error: any) {
+    console.error(error);
+    throw new Error(error.message);
+  }
+};
+
+export const signIn = async (email: string, password: string) => {
+  try {
+    const session: Models.Session = await account.createEmailPasswordSession(
+      email,
+      password
+    );
+    return session;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
+
+export const getCurrentUser = async () => {
+  try {
+    const currentAccount = await account.get();
+    if (!currentAccount) throw Error;
+    const currentUser = await databases.listDocuments(
+      config.databaseId,
+      config.userCollectionId,
+      [Query.equal("accountId", currentAccount.$id)]
+    );
+
+    if (!currentUser) throw Error;
+
+    return currentUser.documents[0];
+  } catch (error) {
+    console.log(error);
+  }
 };
