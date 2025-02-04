ae602210b4b045a213125928cd75c3b752e9571e
nbquochuy <nbquochuy.forwork@gmail.com>
2025-02-04 09:44:56 +0700

    refactor: Update package name and dependencies for Appwrite integration
    This commit updates the package name in app.json to "com.nbquochuy.aora" and adds the necessary dependencies for integrating Appwrite into the Aora app. The appwriteConfig object in lib/appwrite.ts is also updated with the correct endpoint, project ID, database ID, collection IDs, and storage ID. These changes are essential for ensuring proper communication with the Appwrite backend and enabling features such as user management, video storage, and database operations.

---
+++ lib/appwrite.ts
@@ -0,0 +1,9 @@
+export const appwriteConfig = {
+  endpoint: "https://cloud.appwrite.io/v1",
+  platform: "com.nbquochuy.aora",
+  projectId: "67a17a12002fe826ccb0",
+  databaseId: "67a17b6800362cf5607e",
+  userCollectionId: "67a17b900005f630d4bb",
+  videoCollectionId: "67a17c5400330b90bd09",
+  storageId: "67a17db30015bb159891",
+};
