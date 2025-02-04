a75dafed55fa4b0c2f109572eba726ed4d0e965b
nbquochuy <nbquochuy.forwork@gmail.com>
2025-02-04 09:44:25 +0700

    refactor: Update landing page banner with Aora logo
    This commit updates the landing page banner in app/index.tsx to include the Aora logo. The logo is positioned at the bottom right corner of the banner using absolute positioning. This change aims to enhance the visual appeal of the landing page and reinforce the branding of the Aora app.

--- app/index.tsx
+++ app/index.tsx
@@ -22,13 +22,15 @@ export default function App() {
           <View className="relative mt-5">
             <Text className="text-3xl text-white font-bold text-center">
               Discover Endless Possibilities with{" "}
-              <Text className="text-secondary-200">Aora</Text>
+              <Text className="text-secondary-200">
+                Aora
+                <Image
+                  source={images.path}
+                  className="!w-[136px] !h-[15px] absolute -bottom-2 -right-8"
+                  resizeMode="contain"
+                />
+              </Text>
             </Text>
-            <Image
-              source={images.path}
-              className="!w-[136px] !h-[15px] absolute -bottom-2 -right-8"
-              resizeMode="contain"
-            />
           </View>
           <Text className="text-sm font-pregular text-gray-100 text-center mt-7">
             Where creativity meets innovation: embark on a journey of limitless
