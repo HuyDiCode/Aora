62fa8ab1f2d7c81227b9af39886d2fd7792fa774
nbquochuy <nbquochuy.forwork@gmail.com>
2025-02-04 16:02:00 +0700

    refactor: Add GlobalProvider to _layout.tsx for global state management

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { SplashScreen, Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import "../global.css";
+import { GlobalProvider } from "@/context/GlobalProvider";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -31,32 +32,34 @@ const RootLayout = () => {
   }
 
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="(auth)"
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="(tabs)"
-        options={{
-          headerShown: false,
-        }}
-      />
-      {/* <Stack.Screen
+    <GlobalProvider>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="(auth)"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+          }}
+        />
+        {/* <Stack.Screen
         name="/search/[query]"
         options={{
           headerShown: false,
         }}
       /> */}
-    </Stack>
+      </Stack>
+    </GlobalProvider>
   );
 };
 
